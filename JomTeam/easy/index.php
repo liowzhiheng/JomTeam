6ef7cfe7eca964394c5f6ffe5e7abad7d291a885
liow zhi heng <130426114+liowzhiheng@users.noreply.github.com>
2024-11-05 16:27:15 +0800

    Update index.php

--- JomTeam/easy/index.php
+++ JomTeam/easy/index.php
@@ -1,20 +1,125 @@
-	<html>
+<html>
 
-	<head>
-		<title>Login</title>
-		<link rel="stylesheet" href="index.css">
-	</head>
+<head>
+    <title>Login</title>
+    <link rel="stylesheet" href="main.css">
+</head>
 
-	<body>
-		<h1>Welcome To Students' Industrial Training Management System</h1>
-		<div class="container">
-			<form method="post" action="check_login.php">
-				<h2>LOGIN</h2>
-				<div class="input-box"><input type="text" name="username" placeholder="Username" /></div>
-				<div class="input-box"><input type="password" name="password" placeholder="Password" /></div>
-				<p style="text-align: center;"><input type="submit" value="Login" class="button" /></p>
-			</form>
-		</div>
-	</body>
+<body>
 
-	</html>
+    <div style="width: 100%; height: 100%; position: relative; background: white">
+
+        <div
+            style="width: 100%; height: 100%; left: 0px; top: 0px; position: absolute; background: linear-gradient(63deg, rgba(82, 82, 212, 0.86) 28%, #B7DFFD 100%)">
+        </div>
+        <div
+            style="width: 1326px; height: 818px; left: 292px; top: 160px; position: absolute; background: rgba(255, 255, 255, 0.90); box-shadow: 17px 33px 14.399999618530273px 15px rgba(0, 0, 0, 0.25); border-radius: 75px">
+        </div>
+        <div
+            style="width: 437px; height: 47px; padding: 10px; left: 442px; top: 697px; position: absolute; justify-content: flex-start; align-items: center; gap: 10px; display: inline-flex">
+            <div><span
+                    style="color: #131313; font-size: 14px; font-family: Poppins; font-weight: 500; word-wrap: break-word">Not
+                    registered yet?</span><span
+                    style="color: #101540; font-size: 14px; font-family: Poppins; font-weight: 500; word-wrap: break-word">
+                </span><span
+                    style="color: #EB1436; font-size: 14px; font-family: Poppins; font-weight: 500; word-wrap: break-word">Create
+                    a new account</span></div>
+        </div>
+        <div
+            style="width: 496px; height: 124px; left: 439px; top: 420px; position: absolute; color: black; font-size: 20px; font-family: Outfit; font-weight: 400; line-height: 25px; word-wrap: break-word">
+            Find Your Match <br />Discover teammates <br /> <br />Let's turn every game into a winning experience!
+            <br />⚽🏀🏈
+        </div>
+        <div
+            style="left: 428px; top: 296px; position: absolute; color: #EB1436; font-size: 96px; font-family: Raleway; font-weight: 700; word-wrap: break-word">
+            Welcome</div>
+        <img style="width: 587px; height: 551px; left: 969px; top: 289px; position: absolute"
+            src="https://via.placeholder.com/587x551" />
+
+        <!-- start fill-->
+        <form method="post" action="check_login.php">
+            <div class="form-container">
+                <!-- email-->
+                <div>
+                    <input type="text" name="username" placeholder="Email" class="input-field email-field" />
+                </div>
+
+                <!-- password-->
+                <div>
+                    <input type="password" name="password" placeholder="Password" class="input-field password-field" />
+                </div>
+            </div>
+
+            <!-- end fill-->
+
+
+            <!-- start login-->
+            <div class="login-button-wrapper">
+                <div class="login-button-container">
+                    <p>
+                        <input type="submit" value="Login" class="login-button" />
+                    </p>
+                </div>
+            </div>
+            <!-- end login-->
+
+        </form>
+
+        <!-- zuishangmian
+        
+            <div
+                style="width: 1920px; height: 70px; left: 0px; top: 0px; position: absolute; background: white; box-shadow: 0px 5px 0px rgba(0, 0, 0, 0.10)">
+            </div>
+            
+            <div style="width: 624px; height: 35px; left: 70px; top: 18px; position: absolute">
+                <div
+                    style="left: 547px; top: 4px; position: absolute; color: black; font-size: 20px; font-family: PT Sans; font-weight: 400; word-wrap: break-word">
+                    Premium</div>
+                <div
+                    style="left: 53px; top: 4px; position: absolute; color: #FF0000; font-size: 20px; font-family: PT Sans; font-weight: 400; word-wrap: break-word">
+                    Home</div>
+                <div
+                    style="left: 149px; top: 4px; position: absolute; color: black; font-size: 20px; font-family: PT Sans; font-weight: 400; word-wrap: break-word">
+                    Find Match</div>
+                <div
+                    style="left: 447px; top: 4px; position: absolute; color: black; font-size: 20px; font-family: PT Sans; font-weight: 400; word-wrap: break-word">
+                    Profile</div>
+                <div
+                    style="left: 289px; top: 4px; position: absolute; color: black; font-size: 20px; font-family: PT Sans; font-weight: 400; word-wrap: break-word">
+                    Create Match</div>
+                <div
+                    style="left: 0px; top: 0px; position: absolute; color: #EB1436; font-size: 30px; font-family: Raleway; font-style: italic; font-weight: 700; word-wrap: break-word">
+                    S</div>
+            </div>
+            <div style="width: 118.09px; height: 35px; left: 1732px; top: 18px; position: absolute">
+                <div
+                    style="width: 69px; height: 25px; left: 0px; top: 6px; position: absolute; color: black; font-size: 20px; font-family: PT Sans; font-weight: 400; word-wrap: break-word">
+                    Log Out </div>
+                <img style="width: 35px; height: 35px; left: 83.09px; top: 0px; position: absolute"
+                    src="https://via.placeholder.com/35x35" />
+            </div>
+            <div style="width: 35px; height: 38.89px; left: 1677px; top: 18px; position: absolute; background: #2A343D">
+            </div>
+        
+         end zuishangmian-->
+
+        <!-- Navbar -->
+        <div class="navbar">
+            <div class="navbar-menu">
+                <span class="navbar-item logo">S</span>
+                <span class="navbar-item" style="color: #FF0000;">Home</span>
+                <span class="navbar-item">   Find Match</span>
+                <span class="navbar-item">Create Match</span>
+                <span class="navbar-item">Profile</span>
+                <span class="navbar-item">Premium</span>
+            </div>
+            <div class="navbar-user navbar-item logout">
+                <span>Log Out</span>
+                <img src="https://via.placeholder.com/35x35" alt="User">
+            </div>
+        </div>
+
+    </div>
+</body>
+
+</html>
