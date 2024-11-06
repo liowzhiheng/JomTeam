2ad3895416da183923df39be1b84ad2fdb814c24
JUNXIANG <150106653+junxiang22@users.noreply.github.com>
2024-11-06 21:48:01 +0800

    Update create_table.php

--- JomTeam/easy/create_table.php
+++ JomTeam/easy/create_table.php
@@ -4,7 +4,7 @@ require("config.php");
 
 $sqlUser = "CREATE TABLE user (
   id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-  username VARCHAR(255) UNIQUE NOT NULL,
+  email VARCHAR(255) UNIQUE NOT NULL,
   password VARCHAR(255) NOT NULL,
   level INT(3) NOT NULL
 )";
