e5dd6f7a06bbb600feef0c9b5b2fe2d7737428a0
KONG YEW YEONG <148787750+YEWYEONG@users.noreply.github.com>
2024-11-07 17:24:25 +0800

    Add files via upload

---
+++ JomTeam/easy/main.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <link rel="stylesheet" href="main.css">
+</head>
+
+<body>
+
+    <nav class="navbar">
+        <a href="#" class="logo">
+            <img src="IMAGE/S.png" alt="Logo">
+        </a>
+
+        <ul class="menu leftmenu">
+            <li><a href="#home">Home</a></li>
+            <li><a href="#find-match">Find Match</a></li>
+            <li><a href="#create-match">Create Match</a></li>
+            <li><a href="#profile">Profile</a></li>
+            <li><a href="#premium">Premium</a></li>
+        </ul>
+        <ul class="menu rightmenu">
+            <li class="notification"><a href="#notification"><img src="IMAGE/NOTIFICATION.png" alt="Notification"></a>
+            </li>
+            <li class="logout"><a href="#logout">Log out<img src="IMAGE/LOGOUT.png" alt="Logout"></a></li>
+        </ul>
+    </nav>
+
+</body>
+
+</html>
