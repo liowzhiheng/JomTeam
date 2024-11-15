0cf1e84aa6e26f07b496a48a7ee0c2db8307201d
KONG YEW YEONG <kongyewyeong@gmail.com>
2024-11-15 18:46:15 +0800

    NAVBAR

--- create_match.php
+++ create_match.php
@@ -26,9 +26,12 @@ if ($_SESSION["LEVEL"] == 3) {
         ];
     }
     ?>
-    <html>
+    <!DOCTYPE html>
+    <html lang="en">
 
     <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Create Your Own Match</title>
         <link rel="stylesheet" href="create_match.css">
     </head>
@@ -36,9 +39,8 @@ if ($_SESSION["LEVEL"] == 3) {
     <body>
         <nav class="navbar">
             <a href="#" class="logo">
-                <img src="image/jomteam.png" alt="Logo">
+                <img src="IMAGE/jomteam.png" alt="Logo">
             </a>
-
             <ul class="menu leftmenu">
                 <li><a href="main.php">Home</a></li>
                 <li><a href="find_match.php">Find Match</a></li>
@@ -46,13 +48,13 @@ if ($_SESSION["LEVEL"] == 3) {
                 <li><a href="view_profile.php">Profile</a></li>
                 <li><a href="#premium">Premium</a></li>
             </ul>
-
             <ul class="menu rightmenu">
-                <li class="notification"><a href="#notification"><img src="image/NOTIFICATION.png" alt="Notification"></a>
+                <li class="notification"><a href="#notification"><img src="IMAGE/NOTIFICATION.png" alt="Notification"></a>
                 </li>
-                <li class="logout"><a href="login.php">Log out<img src="image/LOGOUT.png" alt="Logout"></a></li>
+                <li class="logout"><a href="login.php">Log out<img src="IMAGE/LOGOUT.png" alt="Logout"></a></li>
             </ul>
         </nav>
+
         <div class="header">
             <h1>Create Your Own Match</h1>
             <br>
