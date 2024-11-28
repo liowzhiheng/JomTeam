12b184ce50f8cc089e30a17af2d1088624f6991d
SOO WEN CHUN <148863733+soowenchun@users.noreply.github.com>
2024-11-28 23:49:49 +0800

    Update main.php

--- main.php
+++ main.php
@@ -3,7 +3,7 @@ session_start(); // Start the PHP session
 
 // Check if the user is logged in
 if ($_SESSION["Login"] != "YES") {
-    header("Location: login.php");
+    header("Location: index.php");
     exit();
 }
 
@@ -84,7 +84,7 @@ $rows = mysqli_fetch_assoc($result);
             <ul class="menu rightmenu">
                 <li class="notification"><a href="#notification"><img src="IMAGE/NOTIFICATION.png" alt="Notification"></a>
                 </li>
-                <li class="logout"><a href="login.php">Log out<img src="IMAGE/LOGOUT.png" alt="Logout"></a></li>
+                <li class="logout"><a href="index.php">Log out<img src="IMAGE/LOGOUT.png" alt="Logout"></a></li>
             </ul>
         </nav>
 
@@ -231,7 +231,7 @@ $rows = mysqli_fetch_assoc($result);
             <ul class="menu rightmenu">
                 <li class="notification"><a href="#notification"><img src="IMAGE/NOTIFICATION.png" alt="Notification"></a>
                 </li>
-                <li class="logout"><a href="login.php">Log out<img src="IMAGE/LOGOUT.png" alt="Logout"></a></li>
+                <li class="logout"><a href="index.php">Log out<img src="IMAGE/LOGOUT.png" alt="Logout"></a></li>
             </ul>
         </nav>
     <?php } ?>
