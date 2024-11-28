a2e0411b170e54dd266930ee7413f280e82a5987
SOO WEN CHUN <148863733+soowenchun@users.noreply.github.com>
2024-11-28 23:48:20 +0800

    Update view_feedback.php

--- view_feedback.php
+++ view_feedback.php
@@ -42,7 +42,7 @@ $result = mysqli_query($conn, $sql);
         <ul class="menu rightmenu">
             <li class="notification"><a href="#notification"><img src="IMAGE/NOTIFICATION.png" alt="Notification"></a>
             </li>
-            <li class="logout"><a href="login.php">Log out<img src="IMAGE/LOGOUT.png" alt="Logout"></a></li>
+            <li class="logout"><a href="index.php">Log out<img src="IMAGE/LOGOUT.png" alt="Logout"></a></li>
         </ul>
     </nav>
 
