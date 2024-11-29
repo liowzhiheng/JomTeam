ac88b4878cc0fa71be16c9e0ce99d0d97d4a2e5f
Ee Ze Qing <eezeqing0219@gmail.com>
2024-11-29 23:53:57 +0800

    Update match_details.php

--- match_details.php
+++ match_details.php
@@ -191,9 +191,9 @@ $has_joined = $checkResult->num_rows > 0;
             <div>
                 <p>You have joined the match.</p>
                 <p>Do you wish to cancel?</p>
-                <a href="join_match.php?id=<?php echo $match_id; ?>">
-                    <button class="joined_button">Joined</button>
-                </a>
+                <button style="width: 20%; height: 70px; font-size: 16px; font-weight: 700; color: white; background: linear-gradient(202deg, #EB1436 0%, rgba(235, 20, 54, 0.66) 71%); border: none; border-radius: 50px; cursor: pointer; transition: background-color 0.3s ease; margin-top: 10%;">
+                    Joined
+                </button>
     </div>
         <?php elseif ($current_players < $max_players): ?>
             <!-- If match is not full and user has not joined -->
