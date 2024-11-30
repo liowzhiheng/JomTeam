fa8232131685fcf813917d9919b7563fa623af60
SOO WEN CHUN <148863733+soowenchun@users.noreply.github.com>
2024-12-01 02:24:08 +0800

    meme

--- create_successful.php
+++ create_successful.php
@@ -10,12 +10,12 @@
 <div class="background"></div>
     <div class="container">
         <h2>You created a match successfully!</h2>
-        <img id="randomImage" src="image/login_done_1.jpg" alt="Create Successful" class="login-image" />
+        <img id="randomImage" src="IMAGE/login_done_1.jpg" alt="Create Successful" class="login-image" />
     </div>
 
     <script>
         document.addEventListener('DOMContentLoaded', function () {
-            const imageFolder = 'image/'; // Set the path to your folder containing images
+            const imageFolder = 'IMAGE/'; // Set the path to your folder containing images
             const images = ['login_done_1.jpg', 'login_done_2.jpg', 'login_done_3.jpg', 'login_done_4.jpg']; // List of images in the folder
             const randomImageElement = document.getElementById('randomImage');
 
