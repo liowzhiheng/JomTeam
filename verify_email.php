680a8d17af1ba83fc3f1fbea94542b637dfeca87
liow zhi heng <130426114+liowzhiheng@users.noreply.github.com>
2024-12-13 16:36:43 +0800

    Update verify_email.php

--- verify_email.php
+++ verify_email.php
@@ -53,12 +53,12 @@
                 // Token is valid, verify the email
                 $sql2 = "UPDATE user SET email_verified = 1 WHERE verification_token = '$token'";
                 if (mysqli_query($conn, $sql2)) {
-                    echo "<h1 class='message'>Email Verified Successfully!</h1>";
+                    echo "<h1 >Email Verified Successfully!</h1>";
                 } else {
-                    echo "<h1 class='message'>Error verifying email. Please try again later.</h1>";
+                    echo "<h1 >Error verifying email. Please try again later.</h1>";
                 }
             } else {
-                echo "<h1 class='message'>Invalid or expired verification link.</h1>";
+                echo "<h1 >Invalid or expired verification link.</h1>";
             }
         } else {
             echo "<h1 class='message'>No token provided.</h1>";
