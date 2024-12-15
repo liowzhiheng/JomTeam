62a5cba4c7d5f17be6de1be7940c955585ac93ee
KONG YEW YEONG <kongyewyeong@gmail.com>
2024-12-15 14:49:02 +0800

    ADS TIME

--- feedback.php
+++ feedback.php
@@ -107,7 +107,7 @@ mysqli_close($conn);
         if (message) {
             setTimeout(() => {
                 message.style.display = 'none';
-            }, 2000);
+            }, 1000);
         }
     </script>
 </body>
