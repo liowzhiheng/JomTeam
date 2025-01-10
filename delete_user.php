89bede8e4262dd9966e1df44dbddd8ec3d7db74d
JUNXIANG <150106653+junxiang22@users.noreply.github.com>
2025-01-10 14:32:41 +0800

    update current player

--- delete_user.php
+++ delete_user.php
@@ -5,6 +5,27 @@ require("config.php");
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $id = $_POST['id'];
 
+    $selectMatch = "SELECT * FROM match_participants WHERE user_id = ?"
+    $result = mysqli_query($conn, $selectMatch);
+    if(mysqli_num_rows($result) > 0){
+        while ($row = $result->fetch_assoc()) {
+            $match_id = $row['match_id'];
+            $selectCurent = "SELECT current_players FROM gamematch WHERE id = $match_id";
+            $result2 = mysqli_query($conn, $selectCurent);
+
+            if (mysqli_num_rows($result2) > 0) {
+                $current = $result2->fetch_assoc();
+            } else {
+                echo "User not found.";
+                exit();
+            }
+            $new_current = $current['current_players'] - 1;
+            $update = "UPDATE gamematch SET current_players = $new_current WHERE id = $match_id";
+            $result3 = mysqli_query($conn, $update);
+        }
+    }else{
+        echo "Match not found";
+    }
     $deleteProfileSql = "DELETE FROM profile WHERE user_id = ?";
     $profileStmt = $conn->prepare($deleteProfileSql);
     $profileStmt->bind_param("i", $id);
@@ -29,6 +50,28 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
 if (isset($_GET['id'])) {
     $id = $_GET['id'];
+
+    $selectMatch = "SELECT * FROM match_participants WHERE user_id = ?"
+    $result = mysqli_query($conn, $selectMatch);
+    if(mysqli_num_rows($result) > 0){
+        while ($row = $result->fetch_assoc()) {
+            $match_id = $row['match_id'];
+            $selectCurent = "SELECT current_players FROM gamematch WHERE id = $match_id";
+            $result2 = mysqli_query($conn, $selectCurent);
+
+            if (mysqli_num_rows($result2) > 0) {
+                $current = $result2->fetch_assoc();
+            } else {
+                echo "User not found.";
+                exit();
+            }
+            $new_current = $current['current_players'] - 1;
+            $update = "UPDATE gamematch SET current_players = $new_current WHERE id = $match_id";
+            $result3 = mysqli_query($conn, $update);
+        }
+    }else{
+        echo "Match not found";
+    }
 
     $deleteProfileSql = "DELETE FROM profile WHERE user_id = ?";
     $profileStmt = $conn->prepare($deleteProfileSql);
