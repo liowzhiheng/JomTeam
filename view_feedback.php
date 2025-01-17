fb01d826befad94daf0caf58b9025174039fd517
JUNXIANG <150106653+junxiang22@users.noreply.github.com>
2025-01-17 14:30:22 +0800

    Update view_feedback.php

--- view_feedback.php
+++ view_feedback.php
@@ -58,6 +58,7 @@ $result = mysqli_query($conn, $sql);
             <li><a href="view_ads.php">Manage Ads</a></li>
             <li><a href="view_match.php">Manage Match</a></li>
             <li><a href="view_feedback.php">Feedback & Report</a></li>
+            <li><a href="view_frame.php">Frame</a></li>
         </ul>
 
         <ul class="menu rightmenu">
